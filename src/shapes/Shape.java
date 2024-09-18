9bfebbf3580f374f3fc5a5fe4259601184abc46f
Anthony Fodor <anthony.fodor@gmail.com>
2024-09-17 22:10:01 -0400

    just clean up an import

--- src/shapes/Shape.java
+++ src/shapes/Shape.java
@@ -4,7 +4,6 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
 import java.io.File;
 
 public abstract class Shape implements Comparable<Shape>
